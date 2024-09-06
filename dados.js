241911a459ad41524efaf9755053af6fc4ee68b9
Maducplx <mariaeduardaalves653@gmail.com>
2024-09-06 09:09:35 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        título: "Rayssa Leal",
+        Descrição: "Rayssa Leal, também conhecida como Fadinha, é uma skatista profissional brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e carisma, Rayssa conquistou o mundo do skate e inspirou uma nova geração de atletas.",
+        Link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        Esporte: "Skate olímpico"
+    },
+    {
+        título: "Rebeca Andrade",
+        Descrição: "Rebeca Andrade é uma ginasta artística brasileira que conquistou diversas medalhas em competições internacionais, incluindo os Jogos Olímpicos. Conhecida por sua força e elegância, ela é um dos maiores nomes da ginástica brasileira.",
+        Link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        Esporte: "Ginástica artística"
+    },
+    {
+        título: "Beatriz Souza",
+        Descrição: "Beatriz Souza é uma judoca brasileira que se destacou em diversas competições nacionais e internacionais. Sua dedicação e técnica a tornaram uma das principais atletas do judô brasileiro.",
+        Link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        Esporte: "Judô"
+    }
+];
